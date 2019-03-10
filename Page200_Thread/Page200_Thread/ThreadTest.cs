e1987c5997b251677da58f9eb4c8a5bbd54234ef
junhong cheon <cjswnsghd12@gmail.com>
2019-03-10 10:48:15 +0900

    Page203_Suspend_Resume

---
+++ Page200_Thread/Page200_Thread/ThreadTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace Page200_Thread
+{
+    class ThreadTest
+    {
+        public void FirstWork()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("F{0} ",i);
+            }
+        }
+
+        public void SecondWork()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("S{0} ", i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadTest t = new ThreadTest();
+
+            Thread first = new Thread(t.FirstWork);
+            Thread second = new Thread(new ThreadStart(t.SecondWork));
+
+            first.Priority = ThreadPriority.Lowest;
+            second.Priority = ThreadPriority.Highest;
+
+            first.Start();
+            second.Start();
+        }
+    }
+}
---
+++ Page203_Suspend_Resume/Page203_Suspend_Resume/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace Page203_Suspend_Resume
+{
+    class Program
+    {
+        public bool sleep = false;
+
+        public void FirstWork()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("F{0}", i);
+                if (i == 5)
+                {
+                    sleep = true;
+                    Console.WriteLine("");
+                    Console.WriteLine("first 쉼...");
+                    Thread.CurrentThread.Suspend();
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program t = new Program();
+            Thread first = new Thread(new ThreadStart(t.FirstWork));
+            first.Start();
+            while (t.sleep == false)
+            {
+                Console.WriteLine("first를 깨웁니다...2초후 깨어 납니다.");
+                Thread.Sleep(2000);
+                first.Resume();
+            }
+        }
+    }
+}
