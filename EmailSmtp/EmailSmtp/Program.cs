a6d49b418aec19a68c229c13ad0c13f4a60f8c58
junhong cheon <cjswnsghd12@gmail.com>
2019-03-10 15:56:24 +0900

    Page250_WinForm

---
+++ EmailSmtp/EmailSmtp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace EmailSmtp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var credentials = new NetworkCredential("vygksthd@gamil.com", "diddl!23");
+
+                var mail = new MailMessage()
+                {
+                    From = new MailAddress("vygksthd@gamil.com"),
+                    Subject = "Test email.",
+                    Body = "Test email body"
+                };
+
+                mail.To.Add(new MailAddress("vygksthd@naver.com"));
+
+                var client = new SmtpClient()
+                {
+                    Port = 587,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Host = "smtp.gmail.com",
+                    EnableSsl = true,
+                    Credentials = credentials
+                };
+                client.Send(mail);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error in sending email: " + ex.Message);
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Email sccessfully sent");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Page250_WinForm/Page250_WinForm/Program.cs
@@ -0,0 +1,45 @@
+﻿//using System;
+//using System.Windows.Forms;
+//namespace Page250_WinForm
+//{
+//    class Program : Form
+//    {
+//        static void Main(string[] args)
+//        {
+//            Program form = new Program();
+//            form.Click += new EventHandler(form.Form_Click);
+//            Console.WriteLine("윈도우 시작..");
+//            Application.Run(form);
+//            Console.WriteLine("윈도우 종료..");
+//        }
+
+//        void Form_Click(object sender, EventArgs e)
+//        {
+//            Console.WriteLine("폼클릭 이벤트..");
+//            Application.Exit();
+//        }
+//    }
+//}
+
+
+using System;
+using System.Windows.Forms;
+namespace Page250_WinForm
+{
+    class Program : Form
+    {
+        static void Main(string[] args)
+        {
+            Program form = new Program();
+            form.Click += new EventHandler(
+                (sender, eventArgs) =>
+                {
+                    Console.WriteLine("폼클릭 이벤트..");
+                    Application.Exit();
+                });
+            Console.WriteLine("윈도우 시작..");
+            Application.Run(form);
+            Console.WriteLine("윈도우 종료..");
+        }        
+    }
+}
