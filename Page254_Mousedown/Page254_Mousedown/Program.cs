2a1b253cdfdd723ea20664ec3a2ab345a6462fea
junhong cheon <cjswnsghd12@gmail.com>
2019-03-10 16:24:32 +0900

    Page254_Mousedown

---
+++ Page254_Mousedown/Page254_Mousedown/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+namespace Page254_Mousedown
+{
+    class Program : Form
+    {
+        public void MouseHandler(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("Sender:" + ((Form)sender).Text);
+            Console.WriteLine("Sender:" + ((Form)sender).Name);
+            Console.WriteLine("X:{0}, Y:{1}", e.X, e.Y);
+            Console.WriteLine("Button:{0}, Click:{1}", e.Button, e.Clicks);
+        }
+
+        public Program(String title)
+        {
+            this.Text = title;
+            this.Name = "윈폼";
+            this.MouseDown += new MouseEventHandler(MouseHandler);
+        }
+
+        static void Main(string[] args)
+        {
+            Application.Run(new Program("마우스 이벤트 예제"));
+        }
+    }
+}
