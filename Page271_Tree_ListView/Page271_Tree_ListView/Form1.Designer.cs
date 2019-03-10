2aa2ba06f1caab4d8722bea3bd37ad012d3936f5
junhong cheon <cjswnsghd12@gmail.com>
2019-03-10 18:18:27 +0900

    Page271_Tree_ListView

---
+++ Page271_Tree_ListView/Page271_Tree_ListView/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Page271_Tree_ListView
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.trv1 = new System.Windows.Forms.TreeView();
+            this.lstv1 = new System.Windows.Forms.ListView();
+            this.addRoot = new System.Windows.Forms.Button();
+            this.addChile = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.addChile);
+            this.groupBox1.Controls.Add(this.addRoot);
+            this.groupBox1.Controls.Add(this.lstv1);
+            this.groupBox1.Controls.Add(this.trv1);
+            this.groupBox1.Location = new System.Drawing.Point(27, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(750, 398);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "TreeView & ListView";
+            // 
+            // trv1
+            // 
+            this.trv1.Location = new System.Drawing.Point(42, 59);
+            this.trv1.Name = "trv1";
+            this.trv1.Size = new System.Drawing.Size(311, 199);
+            this.trv1.TabIndex = 0;
+            // 
+            // lstv1
+            // 
+            this.lstv1.Location = new System.Drawing.Point(404, 59);
+            this.lstv1.Name = "lstv1";
+            this.lstv1.Size = new System.Drawing.Size(311, 199);
+            this.lstv1.TabIndex = 1;
+            this.lstv1.UseCompatibleStateImageBehavior = false;
+            this.lstv1.View = System.Windows.Forms.View.Details;
+            // 
+            // addRoot
+            // 
+            this.addRoot.Location = new System.Drawing.Point(85, 289);
+            this.addRoot.Name = "addRoot";
+            this.addRoot.Size = new System.Drawing.Size(216, 63);
+            this.addRoot.TabIndex = 2;
+            this.addRoot.Text = "루트추가";
+            this.addRoot.UseVisualStyleBackColor = true;
+            this.addRoot.Click += new System.EventHandler(this.addRoot_Click);
+            // 
+            // addChile
+            // 
+            this.addChile.Location = new System.Drawing.Point(452, 289);
+            this.addChile.Name = "addChile";
+            this.addChile.Size = new System.Drawing.Size(216, 63);
+            this.addChile.TabIndex = 3;
+            this.addChile.Text = "자식추가";
+            this.addChile.UseVisualStyleBackColor = true;
+            this.addChile.Click += new System.EventHandler(this.addChile_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmMain";
+            this.Text = "TreeView&ListView TEST";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button addChile;
+        private System.Windows.Forms.Button addRoot;
+        private System.Windows.Forms.ListView lstv1;
+        private System.Windows.Forms.TreeView trv1;
+    }
+}
+
---
+++ Page271_Tree_ListView/Page271_Tree_ListView/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Page271_Tree_ListView
+{
+    public partial class FrmMain : Form
+    {
+        Random r = new Random(37);
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            lstv1.Columns.Add("Name");
+            lstv1.Columns.Add("Depth");
+        }
+
+        void TreeToList()
+        {
+            lstv1.Items.Clear();
+            foreach(TreeNode node in trv1.Nodes)
+            {
+                TreeToList(node);
+            }
+        }
+        
+        void TreeToList(TreeNode node)
+        {
+            lstv1.Items.Add(
+                new ListViewItem(
+                    new string[] { node.Text, node.FullPath.Count(f => f == '\\').ToString() }));
+            foreach (TreeNode n in node.Nodes)
+            {
+                TreeToList(n);
+            }
+        }
+
+        private void addRoot_Click(object sender, EventArgs e)
+        {
+            trv1.Nodes.Add(r.Next().ToString());
+            TreeToList();
+        }
+
+        private void addChile_Click(object sender, EventArgs e)
+        {
+            if(trv1.SelectedNode == null)
+            {
+                MessageBox.Show("선택된 노드 없음.", "트리뷰테스트", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            trv1.SelectedNode.Nodes.Add(r.Next().ToString());
+            trv1.SelectedNode.Expand();
+            TreeToList();
+        }
+    }
+}
